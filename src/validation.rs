a79a07c7111049fdcd1a881787953a5392a06f85
Markus Warne <markus.warne@gmail.com>
2022-12-24 23:43:47 +0100

    add empty spaces to tests

--- src/validation.rs
+++ src/validation.rs
@@ -39,12 +39,14 @@ pub fn is_3x3_valid(area: &[[u8; 3]; 3]) -> bool {
 fn test_is_row_valid() {
     let valid_row1 = [6, 1, 5, 7, 2, 4, 8, 3, 9];
     let valid_row2 = [2, 7, 4, 6, 5, 1, 9, 8, 3];
+    let valid_row3 = [2, 7, 4, 6, 5, 1, 0, 8, 0];
     let invalid_row1 = [2, 7, 1, 6, 5, 1, 9, 8, 3];
     let invalid_row2 = [6, 1, 5, 7, 2, 4, 8, 9, 9];
-    let invalid_row3 = [1, 1, 1, 6, 5, 4, 9, 8, 3];
+    let invalid_row3 = [1, 1, 0, 6, 5, 4, 9, 8, 3];
 
     assert!(is_row_valid(&valid_row1));
     assert!(is_row_valid(&valid_row2));
+    assert!(is_row_valid(&valid_row3));
     assert!(!is_row_valid(&invalid_row1));
     assert!(!is_row_valid(&invalid_row2));
     assert!(!is_row_valid(&invalid_row3));
@@ -54,12 +56,14 @@ fn test_is_row_valid() {
 fn test_is_col_valid() {
     let valid_col1 = [[6], [4], [9], [5], [1], [2], [8], [7], [3]];
     let valid_col2 = [[9], [3], [4], [8], [5], [2], [1], [7], [6]];
+    let valid_col3 = [[0], [3], [0], [8], [5], [2], [0], [7], [6]];
     let invalid_col1 = [[6], [4], [9], [5], [1], [3], [8], [7], [3]];
     let invalid_col2 = [[1], [1], [4], [8], [5], [2], [1], [7], [6]];
-    let invalid_col3 = [[9], [3], [4], [8], [5], [2], [1], [7], [9]];
+    let invalid_col3 = [[9], [3], [4], [8], [5], [2], [1], [0], [9]];
 
     assert!(is_col_valid(&valid_col1));
     assert!(is_col_valid(&valid_col2));
+    assert!(is_col_valid(&valid_col3));
     assert!(!is_col_valid(&invalid_col1));
     assert!(!is_col_valid(&invalid_col2));
     assert!(!is_col_valid(&invalid_col3));
@@ -69,12 +73,14 @@ fn test_is_col_valid() {
 fn test_is_3x3_valid() {
     let valid_3x3_1 = [[6, 1, 5], [4, 8, 7], [9, 2, 3]];
     let valid_3x3_2 = [[8, 3, 9], [1, 6, 2], [5, 7, 4]];
+    let valid_3x3_3 = [[8, 3, 9], [0, 6, 2], [5, 0, 0]];
     let invalid_3x3_1 = [[8, 3, 9], [1, 6, 2], [5, 7, 1]];
     let invalid_3x3_2 = [[8, 3, 9], [1, 6, 2], [9, 7, 4]];
-    let invalid_3x3_3 = [[6, 1, 5], [4, 8, 4], [9, 2, 3]];
+    let invalid_3x3_3 = [[6, 1, 5], [4, 8, 4], [0, 2, 3]];
 
     assert!(is_3x3_valid(&valid_3x3_1));
     assert!(is_3x3_valid(&valid_3x3_2));
+    assert!(is_3x3_valid(&valid_3x3_3));
     assert!(!is_3x3_valid(&invalid_3x3_1));
     assert!(!is_3x3_valid(&invalid_3x3_2));
     assert!(!is_3x3_valid(&invalid_3x3_3));
