120d6a4acac8586270d7b4e0f2bfbaa1b2ec3e96
Markus Warne <markus.warne@gmail.com>
2023-07-30 00:52:58 +0200

    update server port

--- ws.js
+++ ws.js
@@ -3,7 +3,8 @@ ws.onmessage = (d) => {
     console.log(d);
 };
 
-let input = JSON.stringify([
+let input = JSON.stringify(
+  [
       [6, 0, 5, 7, 2, 0, 0, 3, 9],
       [4, 0, 0, 0, 0, 5, 1, 0, 0],
       [0, 2, 0, 1, 0, 0, 0, 0, 4],
@@ -13,6 +14,7 @@ let input = JSON.stringify([
       [8, 0, 0, 0, 0, 3, 0, 2, 0],
       [0, 0, 2, 9, 0, 0, 0, 0, 1],
       [3, 5, 0, 0, 6, 7, 4, 0, 8]
-    ]);
+    ]
+);
 
 // ws.send(input)
