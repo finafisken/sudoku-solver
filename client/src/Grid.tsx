7ca3121af48deb1a4530164d84cff3b71afc6f30
Markus Warne <markus.warne@gmail.com>
2023-07-30 18:08:00 +0200

    improve grid ui

--- client/src/Grid.tsx
+++ client/src/Grid.tsx
@@ -4,12 +4,13 @@ import Cell from './Cell';
 type UpdateCell = (val: number, x: number, y: number) => void
 
 const Grid: Component<{ state: number[][], updateCell: UpdateCell }> = (props) => {
+
 	return (
-		<div class="grid grid-cols-9 gap-4">
+		<div class="grid grid-cols-9 grid-rows-9 rounded-lg overflow-hidden max-w-lg border-4 border-slate-700">
 			<For each={props.state}>
 				{(row, y) => (
 					<For each={row}>
-						{(val, x) => (<Cell val={val} updateCell={v => props.updateCell(v, x(), y())} />)}
+						{(val, x) => (<Cell val={val} x={x()} y={y()} updateCell={props.updateCell} />)}
 					</For>
 				)}
 			</For>
